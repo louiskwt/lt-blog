996a8a8d31bfd2141e286704c750c28d2eab1042
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-15 22:33:13 +0800

    project setup

---
+++ src/Home.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
+function Home() {
+	const [blogs, setBlogs] = useState([
+		{
+			title: 'My new website',
+			body: 'lorem ipsum...',
+			author: 'mario',
+			id: 1
+		},
+		{
+			title: 'Welcome party!',
+			body: 'lorem ipsum...',
+			author: 'yoshi',
+			id: 2
+		},
+		{
+			title: 'Web dev top tips',
+			body: 'lorem ipsum...',
+			author: 'mario',
+			id: 3
+		}
+	]);
+
+	const [name, setName] = useState('mario');
+
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter((blog) => blog.id !== id);
+		setBlogs(newBlogs);
+	};
+
+	useEffect(() => {
+		console.log('use effect ran');
+		console.log(name);
+	}, [name]);
+
+	return (
+		<div className='home'>
+			<BlogList
+				blogs={blogs}
+				title='All Blogs'
+				handleDelete={handleDelete}
+			></BlogList>
+			<BlogList
+				blogs={blogs.filter((blog) => blog.author === 'mario')}
+				title="Mario's blog"
+				handleDelete={handleDelete}
+			></BlogList>
+			<button onClick={() => setName('Ken')}>Change Name</button>
+			<p>{name}</p>
+		</div>
+	);
+}
+
+export default Home;
