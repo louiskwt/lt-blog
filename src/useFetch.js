bf887e5bc76ac8d4bf8c2d0d6554edc426743834
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-16 15:44:00 +0800

    Fetch useEffect clean up

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
 	const [error, setError] = useState(null);
 
 	useEffect(() => {
+		const abortCont = new AbortController();
+
 		setTimeout(() => {
-			fetch(url)
+			fetch(url, { signal: abortCont.signal })
 				.then((res) => {
 					if (!res.ok) {
 						throw Error(
@@ -22,10 +24,15 @@ const useFetch = (url) => {
 					setError(null);
 				})
 				.catch((err) => {
-					setError(err.message);
-					setIsLoading(false);
+					if (err.name === 'AbortError') {
+						console.log('fetch aborted');
+					} else {
+						setError(err.message);
+						setIsLoading(false);
+					}
 				});
 		}, 2000);
+		return () => abortCont.abort();
 	}, [url]);
 	return { data, isLoading, error };
 };
