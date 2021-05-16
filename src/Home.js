a5b2d2960cedae921a639cb9ac65594c0109067f
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-16 14:59:33 +0800

    built custom fuseFetch hook and handle fetch error

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 function Home() {
-	const [blogs, setBlogs] = useState(null);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		setTimeout(() => {
-			fetch('http://localhost:8000/blogs')
-				.then((res) => {
-					return res.json();
-				})
-				.then((data) => {
-					setBlogs(data);
-					setIsLoading(false);
-				});
-		}, 2000);
-	}, []);
+	const { data, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
 	return (
 		<div className='home'>
+			{error && <div>{error}</div>}
 			{isLoading && <div>Loading...</div>}
-			{blogs && <BlogList blogs={blogs} title='All Blogs'></BlogList>}
+			{data && <BlogList blogs={data} title='All Blogs'></BlogList>}
 		</div>
 	);
 }
